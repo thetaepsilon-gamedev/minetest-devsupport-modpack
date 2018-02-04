3918f462492ece7763d85f1d1194b767879ecd43
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-02-04 19:51:26 +0000

    libmthelpers: functional: add fold module

---
+++ libmthelpers/tests/test_fold.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/env lua5.1
+
+local m_fold = mtrequire("com.github.thetaepsilon.minetest.libmthelpers.functional.fold")
+
+local t = { 1, 2, 3, 4, 5 }
+local foldl = m_fold.efoldln(1)
+
+local op = m_fold.op
+
+local r = foldl(m_fold.agen(t), op.add, 0)
+--print(r)
+assert(r:unwrap() == 15)
+
+print("fold tests completed successfully.")
+
