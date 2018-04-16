5f23b0aec2fae6fc457d78b53de81ba822bffe5e
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-04-16 10:27:17 +0100

    libmthelpers.datastructs.matrix: add constructor, index, dimension query and matrix multiplication operations

--- libmthelpers/lib/com/github/thetaepsilon/minetest/libmthelpers/datastructs/matrix.lua
+++ libmthelpers/lib/com/github/thetaepsilon/minetest/libmthelpers/datastructs/matrix.lua
@@ -6,4 +6,151 @@ in the specific case of a 1x3 column vector.
 ]]
 
 local i = {}
+
+local calculate_matrix_index = function(h, w, y, x)
+	-- 1-based indexing strikes again...
+	return (((y - 1) * w) + (x - 1)) + 1
+end
+-- NB: long and wonky on purpose, but may be useful in certain cases.
+i.__calculate_matrix_index = calculate_matrix_index
+
+local getsize = function(self)
+	return self.__y, self.__x
+end
+
+local add_sz = function(matrix, x, y)
+	matrix.__x = x
+	matrix.__y = y
+	matrix.get_size = getsize
+end
+
+-- pre-declare constructor for operations which create new matrices
+local mk_uninit
+
+-- check that a matrix's values are correctly initialised.
+local n = "assert_initialised()"
+local assert_initialised = function(matrix)
+	local height, width = matrix:get_size()
+	local total = height * width
+	for i = 1, total, 1 do
+		assert(type(matrix[i]) == "number",
+			n..": element "..i.."of "..height.."*"..width..
+			" matrix was not a number")
+	end
+	return matrix
+end
+
+-- matrices are typically immutable so this isn't yet a public member.
+local checkinrange = function(upper, v, label)
+	assert(((v >= 1) and (v <= upper)),
+		"matrix "..label.." index out of bounds, expected range " .. 
+		"1-"..upper..", got "..v)
+	return v
+end
+local assign_element = function(matrix, __y, __x, v)
+	local height, width = matrix:get_size()
+	local y = checkinrange(height, __y, "Y")
+	local x = checkinrange(width, __x, "X")
+	matrix[calculate_matrix_index(height, width, y, x)] = v
+end
+
+-- bounds-checking index operation
+local index = function(self, __y, __x)
+	local height, width = self:get_size()
+	local y = checkinrange(height, __y, "Y")
+	local x = checkinrange(width, __x, "X")
+	local v = self[calculate_matrix_index(height, width, y, x)]
+	assert(v ~= nil, "insanity condition: matrix value was nil")
+	return v
+end
+
+local err_wrong_size = "incompatible matrix dimensions: "
+local matrix_multiply = function(self, other)
+	--[[
+	       [ b b b b ]
+	       [ b b b b ]
+	[ a a ][ c c c c ]
+	[ a a ][ c c c c ]
+	[ a a ][ c c c c ]
+	]]
+	local ha, wa = self:get_size()
+	local hb, wb = other:get_size()
+	assert(wa == hb,
+		err_wrong_size.."matrix A of width " .. wa ..
+		" expected B of same height, got "..hb)
+	-- number of multiply-then-add operations per matrix element
+	local oplen = wa
+
+	local wr = wb
+	local hr = ha
+	local result = mk_uninit(hr, wr)
+	for y = 1, hr, 1 do
+		for x = 1, wr, 1 do
+			local total = 0
+			for i = 1, oplen, 1 do
+				local v = (self:index(y, i) * other:index(i, x))
+				total = total + v
+			end
+			assign_element(result, y, x, total)
+		end
+	end
+	return assert_initialised(result)
+end
+
+
+
+
+
+local add_methods = function(matrix, x, y)
+	-- get_size(): returns height, width (*in that order*)
+	add_sz(matrix, x, y)
+	-- index accessor
+	matrix.index = index
+	-- multiply_m: matrix multiplication (throws if sizes incompatible)
+	matrix.multiply_m = matrix_multiply
+end
+
+local msg_dim = " dimension for matrix should be positive"
+-- NB: matrices are specified in number of rows first!
+mk_uninit = function(y, x)
+	assert(x > 0, "x"..msg_dim)
+	assert(y > 0, "y"..msg_dim)
+
+	local result = {}
+	add_methods(result, x, y)
+	return result
+end
+
+mk = function(y, x, values)
+	local result = mk_uninit(y, x)
+
+	local size = x * y
+	local desc = x .. "*" .. y
+	local actual = #values
+	assert(actual == size, desc.." matrix requires "..size.." values, got "..actual)
+	for i = 1, x*y, 1 do
+		result[i] = values[i]
+	end
+
+	return assert_initialised(result)
+end
+i.new = mk
+
+-- primitive display routine, can't always guarantee a monospace terminal output.
+i.primitive_print = function(matrix, printer)
+	local height, width = matrix:get_size()
+	for y = 1, height, 1 do
+		local notfirst = false
+		local row = ""
+		for x = 1, width, 1 do
+			--print(y, x)
+			local sep = notfirst and ", " or ""
+			row = row .. sep .. matrix:index(y, x)
+			notfirst = true
+		end
+		printer(row)
+	end
+end
+
 return i
+
