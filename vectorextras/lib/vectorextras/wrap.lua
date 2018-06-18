ffb1656ece344175c963c3558f0578ed49231272
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-06-18 19:19:52 +0100

    vectorextras: [new code] add wrap routine to convert back to MT XYZ tables

---
+++ vectorextras/lib/vectorextras/wrap.lua
@@ -0,0 +1,16 @@
+--[[
+The counterpart to unwrap.lua,
+effectively vector.new with some sanity checks on types.
+]]
+local assert_num = function(v)
+	assert(type(v) == "number")
+	return v
+end
+local wrap = function(_x, _y, _z)
+	return {
+		x = assert_num(_x),
+		y = assert_num(_y),
+		z = assert_num(_z),
+	}
+end
+
