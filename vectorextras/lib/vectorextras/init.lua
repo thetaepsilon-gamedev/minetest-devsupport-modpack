4a64a68f7d2b5fa985f00d807ea2787750adc1b8
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-06-18 21:25:17 +0100

    vectorextras: [new code] add parent namespace

---
+++ vectorextras/lib/vectorextras/init.lua
@@ -0,0 +1,15 @@
+local components = {
+	"unwrap",
+	"wrap",
+	"display",
+	"magnitude",
+	"add",
+	"subtract",
+	"scalar_multiply",
+	"dotproduct",
+	"reflect",
+}
+
+
+
+return modns.mk_parent_ns(components)
