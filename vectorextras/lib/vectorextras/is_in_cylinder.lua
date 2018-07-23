c4e4cda68ca1d540d804014494739486b12974e4
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-07-23 15:36:10 +0100

    vectorextras: [new code] add is_in_cylinder routine and start of test cases

---
+++ vectorextras/lib/vectorextras/is_in_cylinder.lua
@@ -0,0 +1,59 @@
+--[[
+Work out whether a given point P lies within a specified cylinder.
+This cylinder is specified as two points A and B,
+indicating the centre points of the two circular ends,
+and a radius of the cross section.
+The leaf functions (after partial application) below return a boolean value,
+where true indicates inside the cylinder and false is not so.
+]]
+
+local bp = "ds2.minetest.vectorextras."
+local m_reflect = mtrequire(bp.."reflect")
+local m_sub = mtrequire(bp.."subtract")
+local sub = m_sub.raw
+local m_unit = mtrequire(bp.."unit")
+local normal = m_unit.raw
+local m_len = mtrequire(bp.."magnitude")
+local dist2 = m_len.squared_raw
+
+-- we use the reflect routine to "flatten" points
+-- into a plane perpendicular to vector AB.
+-- we need this as the circular end of the cylinder lies in this plane;
+-- we can then use this to work out distance from A to this projected point,
+-- and therefore determine if it lies within the radius
+-- of the cross-sectional circle (I really need some diagrams...)
+local flatten = m_reflect.mk_reflect_raw_({getdot=true,reflectivity=-1})
+
+local i = {}
+local cylinder_test_raw = function(ax, ay, az, _bx, _by, _bz, radius, _px, _py, _pz)
+	-- firstly, we rebase the coordinates to be relative to A.
+	-- this way, the length is implict in B, and makes it easier to work with P.
+	local dx, dy, dz = sub(_bx, _by, _bz, ax, ay, az)
+	local px, py, pz = sub(_px, _py, _pz, ax, ay, az)
+
+	-- next, get normalised vector and length for use in reflect.
+	-- the length represents the max height of the cylinder,
+	-- which we will need later.
+	local nx, ny, nz, maxh = normal(dx, dy, dz)
+
+	-- shove the point into the plane and get it's "height" above the plane.
+	local fx, fy, fz, height = flatten(px, py, pz, nx, ny, nz)
+	-- y = 0 of the cylinder is implicitly height = 0 due to rebasing.
+	-- height is below cylinder or above max height? fail
+	if height < 0 or height > maxh then return false end
+
+	-- work out the distance from the flattened point to the origin (A).
+	-- if this is greater than the cylinder's radius, fail
+	local r2 = radius * radius
+	-- if a < b then sqrt(a) < sqrt(b), sqrt is redundant
+	if dist2(fx, fy, fz) > r2 then return false end
+
+	-- otherwise we should be good
+	return true
+end
+i.raw = cylinder_test_raw
+
+
+
+return i
+
---
+++ vectorextras/tests/test_is_in_cylinder.lua
@@ -0,0 +1,48 @@
+local m_cyl = mtrequire("ds2.minetest.vectorextras.is_in_cylinder")
+local is_in_cylinder = m_cyl.raw
+
+local test_bounds = function(...)
+	assert(is_in_cylinder(...))
+end
+local mkbounds = function(ax, ay, az, bx, by, bz, radius)
+	local test = function(px, py, pz)
+		return is_in_cylinder(ax, ay, az, bx, by, bz, radius, px, py, pz)
+	end
+	local accept = function(...)
+		assert(test(...))
+	end
+	local reject = function(...)
+		assert(not test(...))
+	end
+	return accept, reject
+end
+
+-- a simple cylinder of height and radius one.
+-- the origin ought to lie inside this point
+local r = 1
+local accept, reject = mkbounds(0, 0, 0, 0, 1, 0, r)
+accept(0, 0, 0)
+-- as should the top point
+accept(0, 1, 0)
+-- and the mid-point
+accept(0, 0.5, 0)
+-- points just outside this height range should NOT work
+reject(0, -0.1, 0)
+reject(0, -1, 0)
+reject(0, 1.1, 0)
+reject(0, 2.0, 0)
+
+-- point that will lie off-centre but within the circle should work.
+accept(1.0, 0, 0)
+accept(0, 0, 1.0)
+accept(-0.5, 0, -0.5)
+-- however, points outside that circle should NOT work
+reject(1.0, 0, 1.0)
+reject(-1, 0, -1)
+-- circular cross section should not change with height
+accept(1.0, 1, 0)
+accept(0, 1, 1.0)
+accept(-0.5, 1, -0.5)
+reject(1.0, 1, 1.0)
+reject(-1, 1, -1)
+
