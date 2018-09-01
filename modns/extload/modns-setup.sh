426e3c209015fe8efd73efc9218fba45c1e96480
thetaepsilon <thetaepsilon@noreply.users.github.com>
2018-09-01 16:23:29 +0100

    modns: extload/modns-setup.sh: [enhancement] allow main home directory to be overridden

--- modns/extload/modns-setup.sh
+++ modns/extload/modns-setup.sh
@@ -1,8 +1,12 @@
+if test -z "$MINETEST_HOME"; then {
+	export MINETEST_HOME="$HOME";
+}; fi;
+
 if test -z "$MINETEST_MOD_HOME"; then {
-	export MINETEST_MOD_HOME="$HOME/.minetest/mods";
+	export MINETEST_MOD_HOME="$MINETEST_HOME/.minetest/mods";
 }; fi;
 export MODNS_PATH="$MINETEST_MOD_HOME/devsupport-modpack/modns";
 export OSDIRSEP="/";
-export MODNS_LOADER_DATA="$HOME/.config/mt_modns_extdata";
+export MODNS_LOADER_DATA="$MINETEST_HOME/.config/mt_modns_extdata";
 export LUA_INIT="@$MODNS_PATH/extload/init.lua";
 
